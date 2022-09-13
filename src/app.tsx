e6939624bf1296ef3779e69bea61569a80a377ba
a-wing <1@233.email>
2022-09-13 20:24:41 +0800

    :hammer: refactor enabled button

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,22 @@ import Sample from 'lodash.sample'
 import Word from './data'
 import Question from './question'
 
+function EnableButton(props: { name: keyof typeof Word, words: { name: string }[], setWords: (_: any) => void }) {
+  const [enabled, setEnabled] = useState(false)
+  const { name, words, setWords } = props
+  return (
+    <>
+      <input type="checkbox" checked={ enabled } onChange={ () => {
+        enabled
+          ? setWords(words.filter(i => !Word[name].find(j => i.name === j.name) ))
+          : setWords([...words, ...Word[name]])
+        setEnabled(!enabled)
+      } } />
+      <label>{ name }</label>
+    </>
+  )
+}
+
 function App() {
   const [words, setWords] = useState(Word.a)
 
@@ -18,15 +34,6 @@ function App() {
     setQuestion(next())
   }, [words])
 
-  const [lineKa, setLineKa] = useState(false)
-  const [lineSa, setLineSa] = useState(false)
-  const [lineTa, setLineTa] = useState(false)
-  const [lineNa, setLineNa] = useState(false)
-  const [lineHa, setLineHa] = useState(false)
-  const [lineMa, setLineMa] = useState(false)
-  const [lineYa, setLineYa] = useState(false)
-  const [lineRa, setLineRa] = useState(false)
-  const [lineWa, setLineWa] = useState(false)
   return (
     <div className="App">
       <h1>这回一定会熟练掌握日语五十音</h1>
@@ -34,78 +41,7 @@ function App() {
 
       <hr/>
 
-      <input type="checkbox" checked={ lineKa } onChange={ () => {
-        lineKa
-          ? setWords(words.filter(i => !Word.ka.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ka])
-        setLineKa(!lineKa)
-      } } />
-      <label>ka</label>
-
-      <input type="checkbox" checked={ lineSa } onChange={ () => {
-        lineSa
-          ? setWords(words.filter(i => !Word.sa.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.sa])
-        setLineSa(!lineSa)
-      } } />
-      <label>sa</label>
-
-      <input type="checkbox" checked={ lineTa } onChange={ () => {
-        lineTa
-          ? setWords(words.filter(i => !Word.ta.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ta])
-        setLineTa(!lineTa)
-      } } />
-      <label>ta</label>
-
-      <input type="checkbox" checked={ lineNa } onChange={ () => {
-        lineNa
-          ? setWords(words.filter(i => !Word.na.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.na])
-        setLineNa(!lineNa)
-      } } />
-      <label>na</label>
-
-      <input type="checkbox" checked={ lineHa } onChange={ () => {
-        lineHa
-          ? setWords(words.filter(i => !Word.ha.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ha])
-        setLineHa(!lineHa)
-      } } />
-      <label>ha</label>
-
-      <input type="checkbox" checked={ lineMa } onChange={ () => {
-        lineMa
-          ? setWords(words.filter(i => !Word.ma.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ma])
-        setLineMa(!lineMa)
-      } } />
-      <label>ma</label>
-
-      <input type="checkbox" checked={ lineYa } onChange={ () => {
-        lineYa
-          ? setWords(words.filter(i => !Word.ya.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ya])
-        setLineYa(!lineYa)
-      } } />
-      <label>ya</label>
-
-      <input type="checkbox" checked={ lineRa } onChange={ () => {
-        lineRa
-          ? setWords(words.filter(i => !Word.ra.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.ra])
-        setLineRa(!lineRa)
-      } } />
-      <label>ra</label>
-
-      <input type="checkbox" checked={ lineWa } onChange={ () => {
-        lineWa
-          ? setWords(words.filter(i => !Word.wa.find(j => i.name === j.name) ))
-          : setWords([...words, ...Word.wa])
-        setLineWa(!lineWa)
-      } } />
-      <label>wa</label>
-
+      { Object.keys(Word).map(i => <EnableButton key={ i } name={ i as keyof typeof Word } words={ words } setWords={ (d) => setWords(d) } />) }
     </div>
   )
 }
