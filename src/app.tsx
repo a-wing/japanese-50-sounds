3537f035ebf2483f01850a0c1e3c9ba54b7cc0f9
a-wing <1@233.email>
2022-09-13 15:33:24 +0800

    :books: add data [ta,na,ha,ma,ya,ra,wa,n]

--- src/app.tsx
+++ src/app.tsx
@@ -20,6 +20,13 @@ function App() {
 
   const [lineKa, setLineKa] = useState(false)
   const [lineSa, setLineSa] = useState(false)
+  const [lineTa, setLineTa] = useState(false)
+  const [lineNa, setLineNa] = useState(false)
+  const [lineHa, setLineHa] = useState(false)
+  const [lineMa, setLineMa] = useState(false)
+  const [lineYa, setLineYa] = useState(false)
+  const [lineRa, setLineRa] = useState(false)
+  const [lineWa, setLineWa] = useState(false)
   return (
     <div className="App">
       <h1>这回一定会熟练掌握日语五十音</h1>
@@ -42,6 +49,63 @@ function App() {
         setLineSa(!lineSa)
       } } />
       <label>sa</label>
+
+      <input type="checkbox" checked={ lineTa } onChange={ () => {
+        lineTa
+          ? setWords(words.filter(i => !Word.ta.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.ta])
+        setLineTa(!lineTa)
+      } } />
+      <label>ta</label>
+
+      <input type="checkbox" checked={ lineNa } onChange={ () => {
+        lineNa
+          ? setWords(words.filter(i => !Word.na.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.na])
+        setLineNa(!lineNa)
+      } } />
+      <label>na</label>
+
+      <input type="checkbox" checked={ lineHa } onChange={ () => {
+        lineHa
+          ? setWords(words.filter(i => !Word.ha.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.ha])
+        setLineHa(!lineHa)
+      } } />
+      <label>ha</label>
+
+      <input type="checkbox" checked={ lineMa } onChange={ () => {
+        lineMa
+          ? setWords(words.filter(i => !Word.ma.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.ma])
+        setLineMa(!lineMa)
+      } } />
+      <label>ma</label>
+
+      <input type="checkbox" checked={ lineYa } onChange={ () => {
+        lineYa
+          ? setWords(words.filter(i => !Word.ya.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.ya])
+        setLineYa(!lineYa)
+      } } />
+      <label>ya</label>
+
+      <input type="checkbox" checked={ lineRa } onChange={ () => {
+        lineRa
+          ? setWords(words.filter(i => !Word.ra.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.ra])
+        setLineRa(!lineRa)
+      } } />
+      <label>ra</label>
+
+      <input type="checkbox" checked={ lineWa } onChange={ () => {
+        lineWa
+          ? setWords(words.filter(i => !Word.wa.find(j => i.name === j.name) ))
+          : setWords([...words, ...Word.wa])
+        setLineWa(!lineWa)
+      } } />
+      <label>wa</label>
+
     </div>
   )
 }
