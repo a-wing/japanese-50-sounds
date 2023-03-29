4c7a6dc4704c62a3c324885ac29f760f640d46be
a-wing <1@233.email>
2023-03-29 23:58:03 +0800

    :rocket: add dakuon #2

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
+import CloneDeep from 'lodash.clonedeep'
 import './app.css'
 
-import Word from './data'
+import Table from './data'
 import { Flags } from './lib'
 import Question from './question'
+
+let Data = Object.values(CloneDeep(Table)).reduce((init, item) => Object.assign(init, item))
 
 function EnableFlags(props: { name: string, flags: string[], setFlags: (_: string[]) => void }) {
   const { name, flags, setFlags } = props
@@ -21,7 +24,14 @@ function EnableFlags(props: { name: string, flags: string[], setFlags: (_: strin
   )
 }
 
-function EnableButton(props: { name: keyof typeof Word, words: Map<string, any>, setWords: (_: any) => void }) {
+type Word = {
+  name: string,
+  romaji: string,
+  hiragana: string,
+  katakana: string,
+}
+
+function EnableButton(props: { name: keyof typeof Data, words: Map<string, Word>, setWords: (_: any) => void }) {
   const { name, words, setWords } = props
   const [enabled, setEnabled] = useState(words.has(name))
   return (
@@ -29,7 +39,7 @@ function EnableButton(props: { name: keyof typeof Word, words: Map<string, any>,
       <input type="checkbox" checked={ enabled } onChange={ () => {
         enabled
           ? words.delete(name)
-          : words.set(name, Word[name])
+          : words.set(name, Data[name])
 
         setWords(Object.fromEntries(words))
         setEnabled(!enabled)
@@ -39,11 +49,22 @@ function EnableButton(props: { name: keyof typeof Word, words: Map<string, any>,
   )
 }
 
-function App() {
-  const [words, setWords] = useState(Word)
-  const [flags, setFlags] = useState<string[]>(Flags)
+function Item(props: {
+  words: any,
+  mapWord: any,
+  setWords: (_: any) => void,
+}) {
+  return (
+    <>
+      { Object.keys(props.words).map(i => <EnableButton key={ i } name={ i as keyof typeof Data } words={ props.mapWord } setWords={ d => props.setWords(d) } />) }
+    </>
+  )
+}
 
-  const mapWord = useRef(new Map(Object.entries(Word)))
+function App() {
+  const [words, setWords] = useState(Data)
+  const [flags, setFlags] = useState<string[]>(Flags)
+  const mapWord = useRef(new Map(Object.entries(Data)))
 
   return (
     <div className="App">
@@ -53,8 +74,14 @@ function App() {
       <hr/>
       { Flags.map(i => <EnableFlags key={ i } name={ i } flags={ flags } setFlags={ a => setFlags(a) } />) }
       <hr/>
+      清音：<Item words={ Table['gojuuon'] } mapWord={ mapWord.current } setWords={ (d: any) => setWords(d) } ></Item>
+      <hr/>
+      浊音：<Item words={ Table['dakuon'] } mapWord={ mapWord.current } setWords={ (d: any) => setWords(d) } ></Item>
+      &nbsp;
+      半浊音：<Item words={ Table['handakuon'] } mapWord={ mapWord.current } setWords={ (d: any) => setWords(d) } ></Item>
+      <hr/>
+      拗音：<Item words={ Table['yoon'] } mapWord={ mapWord.current } setWords={ (d: any) => setWords(d) } ></Item>
 
-      { Object.keys(Word).map(i => <EnableButton key={ i } name={ i as keyof typeof Word } words={ mapWord.current } setWords={ (d) => setWords(d) } />) }
     </div>
   )
 }
--- src/data.ts
+++ src/data.ts
@@ -1,242 +1,161 @@
 export default {
-  a: [{
-    name: "a",
-    romaji: "a",
-    hiragana: "あ",
-    katakana: "ア",
-  }, {
-    name: "i",
-    romaji: "i",
-    hiragana: "い",
-    katakana: "イ",
-  }, {
-    name: "u",
-    romaji: "u",
-    hiragana: "う",
-    katakana: "ウ",
-  }, {
-    name: "e",
-    romaji: "e",
-    hiragana: "え",
-    katakana: "エ",
-  }, {
-    name: "o",
-    romaji: "o",
-    hiragana: "お",
-    katakana: "オ",
-  }],
-  ka: [{
-    name: "ka",
-    romaji: "ka",
-    hiragana: "か",
-    katakana: "カ",
-  }, {
-    name: "ki",
-    romaji: "ki",
-    hiragana: "き",
-    katakana: "キ",
-  }, {
-    name: "ku",
-    romaji: "ku",
-    hiragana: "く",
-    katakana: "ク",
-  }, {
-    name: "ke",
-    romaji: "ke",
-    hiragana: "け",
-    katakana: "ケ",
-  }, {
-    name: "ko",
-    romaji: "ko",
-    hiragana: "こ",
-    katakana: "コ",
-  }],
-  sa: [{
-    name: "sa",
-    romaji: "sa",
-    hiragana: "さ",
-    katakana: "サ",
-  }, {
-    name: "si",
-    romaji: "si (shi)",
-    hiragana: "し",
-    katakana: "シ",
-  }, {
-    name: "su",
-    romaji: "su",
-    hiragana: "す",
-    katakana: "ス",
-  }, {
-    name: "se",
-    romaji: "se",
-    hiragana: "せ",
-    katakana: "セ",
-  }, {
-    name: "so",
-    romaji: "so",
-    hiragana: "そ",
-    katakana: "ソ",
-  }],
-  ta: [{
-    name: "ta",
-    romaji: "ta",
-    hiragana: "た",
-    katakana: "タ",
-  }, {
-    name: "ti",
-    romaji: "ti (chi)",
-    hiragana: "ち",
-    katakana: "チ",
-  }, {
-    name: "tu",
-    romaji: "tu (tsu)",
-    hiragana: "つ",
-    katakana: "ツ",
-  }, {
-    name: "te",
-    romaji: "te",
-    hiragana: "て",
-    katakana: "テ",
-  }, {
-    name: "to",
-    romaji: "to",
-    hiragana: "と",
-    katakana: "ト",
-  }],
-  na: [{
-    name: "na",
-    romaji: "na",
-    hiragana: "な",
-    katakana: "ナ",
-  }, {
-    name: "ni",
-    romaji: "ni",
-    hiragana: "に",
-    katakana: "ニ",
-  }, {
-    name: "nu",
-    romaji: "nu",
-    hiragana: "ぬ",
-    katakana: "ヌ",
-  }, {
-    name: "ne",
-    romaji: "ne",
-    hiragana: "ね",
-    katakana: "ネ",
-  }, {
-    name: "no",
-    romaji: "no",
-    hiragana: "の",
-    katakana: "ノ",
-  }],
-  ha: [{
-    name: "ha",
-    romaji: "ha",
-    hiragana: "は",
-    katakana: "ハ",
-  }, {
-    name: "hi",
-    romaji: "hi",
-    hiragana: "ひ",
-    katakana: "ヒ",
-  }, {
-    name: "hu",
-    romaji: "hu (fu)",
-    hiragana: "ふ",
-    katakana: "フ",
-  }, {
-    name: "he",
-    romaji: "he",
-    hiragana: "へ",
-    katakana: "ヘ",
-  }, {
-    name: "ho",
-    romaji: "ho",
-    hiragana: "ほ",
-    katakana: "ホ",
-  }],
-  ma: [{
-    name: "ma",
-    romaji: "ma",
-    hiragana: "ま",
-    katakana: "マ",
-  }, {
-    name: "mi",
-    romaji: "mi",
-    hiragana: "み",
-    katakana: "ミ",
-  }, {
-    name: "mu",
-    romaji: "mu",
-    hiragana: "む",
-    katakana: "ム",
-  }, {
-    name: "me",
-    romaji: "me",
-    hiragana: "め",
-    katakana: "メ",
-  }, {
-    name: "mo",
-    romaji: "mo",
-    hiragana: "も",
-    katakana: "モ",
-  }],
-  ya: [{
-    name: "ya",
-    romaji: "ya",
-    hiragana: "や",
-    katakana: "ヤ",
-  }, {
-    name: "yu",
-    romaji: "yu",
-    hiragana: "ゆ",
-    katakana: "ユ",
-  }, {
-    name: "yo",
-    romaji: "yo",
-    hiragana: "よ",
-    katakana: "ヨ",
-  }],
-  ra: [{
-    name: "ra",
-    romaji: "ra",
-    hiragana: "ら",
-    katakana: "ラ",
-  }, {
-    name: "ri",
-    romaji: "ri",
-    hiragana: "り",
-    katakana: "リ",
-  }, {
-    name: "ru",
-    romaji: "ru",
-    hiragana: "る",
-    katakana: "ル",
-  }, {
-    name: "re",
-    romaji: "re",
-    hiragana: "れ",
-    katakana: "レ",
-  }, {
-    name: "ro",
-    romaji: "ro",
-    hiragana: "ろ",
-    katakana: "ロ",
-  }],
-  wa: [{
-    name: "wa",
-    romaji: "wa",
-    hiragana: "わ",
-    katakana: "ワ",
-  }, {
-    name: "wo",
-    romaji: "wo",
-    hiragana: "を",
-    katakana: "ヲ",
-  }, {
-    name: "n",
-    romaji: "n",
-    hiragana: "ん",
-    katakana: "ン",
-  }],
+  gojuuon: {
+    a: [
+      { name: "a", hiragana: "あ", katakana: "ア", romaji: "a" },
+      { name: "i", hiragana: "い", katakana: "イ", romaji: "i" },
+      { name: "u", hiragana: "う", katakana: "ウ", romaji: "u" },
+      { name: "e", hiragana: "え", katakana: "エ", romaji: "e" },
+      { name: "o", hiragana: "お", katakana: "オ", romaji: "o" },
+    ],
+    ka: [
+      { name: "ka", hiragana: "か", katakana: "カ", romaji: "ka" },
+      { name: "ki", hiragana: "き", katakana: "キ", romaji: "ki" },
+      { name: "ku", hiragana: "く", katakana: "ク", romaji: "ku" },
+      { name: "ke", hiragana: "け", katakana: "ケ", romaji: "ke" },
+      { name: "ko", hiragana: "こ", katakana: "コ", romaji: "ko" },
+    ],
+    sa: [
+      { name: "sa", hiragana: "さ", katakana: "サ", romaji: "sa" },
+      { name: "si", hiragana: "し", katakana: "シ", romaji: "si (shi)" },
+      { name: "su", hiragana: "す", katakana: "ス", romaji: "su" },
+      { name: "se", hiragana: "せ", katakana: "セ", romaji: "se" },
+      { name: "so", hiragana: "そ", katakana: "ソ", romaji: "so" },
+    ],
+    ta: [
+      { name: "ta", hiragana: "た", katakana: "タ", romaji: "ta" },
+      { name: "ti", hiragana: "ち", katakana: "チ", romaji: "ti (chi)" },
+      { name: "tu", hiragana: "つ", katakana: "ツ", romaji: "tu (tsu)" },
+      { name: "te", hiragana: "て", katakana: "テ", romaji: "te" },
+      { name: "to", hiragana: "と", katakana: "ト", romaji: "to" },
+    ],
+    na: [
+      { name: "na", hiragana: "な", katakana: "ナ", romaji: "na" },
+      { name: "ni", hiragana: "に", katakana: "ニ", romaji: "ni" },
+      { name: "nu", hiragana: "ぬ", katakana: "ヌ", romaji: "nu" },
+      { name: "ne", hiragana: "ね", katakana: "ネ", romaji: "ne" },
+      { name: "no", hiragana: "の", katakana: "ノ", romaji: "no" },
+    ],
+    ha: [
+      { name: "ha", hiragana: "は", katakana: "ハ", romaji: "ha" },
+      { name: "hi", hiragana: "ひ", katakana: "ヒ", romaji: "hi" },
+      { name: "hu", hiragana: "ふ", katakana: "フ", romaji: "hu (fu)" },
+      { name: "he", hiragana: "へ", katakana: "ヘ", romaji: "he" },
+      { name: "ho", hiragana: "ほ", katakana: "ホ", romaji: "ho" },
+    ],
+    ma: [
+      { name: "ma", hiragana: "ま", katakana: "マ", romaji: "ma" },
+      { name: "mi", hiragana: "み", katakana: "ミ", romaji: "mi" },
+      { name: "mu", hiragana: "む", katakana: "ム", romaji: "mu" },
+      { name: "me", hiragana: "め", katakana: "メ", romaji: "me" },
+      { name: "mo", hiragana: "も", katakana: "モ", romaji: "mo" },
+    ],
+    ya: [
+      { name: "ya", hiragana: "や", katakana: "ヤ", romaji: "ya" },
+      { name: "yu", hiragana: "ゆ", katakana: "ユ", romaji: "yu" },
+      { name: "yo", hiragana: "よ", katakana: "ヨ", romaji: "yo" },
+    ],
+    ra: [
+      { name: "ra", hiragana: "ら", katakana: "ラ", romaji: "ra" },
+      { name: "ri", hiragana: "り", katakana: "リ", romaji: "ri" },
+      { name: "ru", hiragana: "る", katakana: "ル", romaji: "ru" },
+      { name: "re", hiragana: "れ", katakana: "レ", romaji: "re" },
+      { name: "ro", hiragana: "ろ", katakana: "ロ", romaji: "ro" },
+    ],
+    wa: [
+      { name: "wa", hiragana: "わ", katakana: "ワ", romaji: "wa" },
+      { name: "wo", hiragana: "を", katakana: "ヲ", romaji: "wo" },
+      { name: "n", hiragana: "ん", katakana: "ン", romaji: "n" },
+    ],
+  },
+  dakuon: {
+    ga: [
+      { name: "ga", hiragana: "が", katakana: "ガ", romaji: "ga" },
+      { name: "gi", hiragana: "ぎ", katakana: "ギ", romaji: "gi" },
+      { name: "gu", hiragana: "ぐ", katakana: "グ", romaji: "gu" },
+      { name: "ge", hiragana: "げ", katakana: "ゲ", romaji: "ge" },
+      { name: "go", hiragana: "ご", katakana: "ゴ", romaji: "go" },
+    ],
+    za: [
+      { name: "za", hiragana: "ざ", katakana: "ザ", romaji: "za" },
+      { name: "zi", hiragana: "じ", katakana: "ジ", romaji: "zi (ji)" },
+      { name: "zu", hiragana: "ず", katakana: "ズ", romaji: "zu" },
+      { name: "ze", hiragana: "ぜ", katakana: "ゼ", romaji: "ze" },
+      { name: "zo", hiragana: "ぞ", katakana: "ゾ", romaji: "zo" },
+    ],
+    da: [
+      { name: "da", hiragana: "だ", katakana: "ダ", romaji: "da" },
+      { name: "di", hiragana: "ぢ", katakana: "ヂ", romaji: "di (ji)" },
+      { name: "du", hiragana: "づ", katakana: "ヅ", romaji: "du (zu)" },
+      { name: "de", hiragana: "で", katakana: "デ", romaji: "de" },
+      { name: "do", hiragana: "ど", katakana: "ド", romaji: "do" },
+    ],
+    ba: [
+      { name: "ba", hiragana: "ば", katakana: "バ", romaji: "ba" },
+      { name: "bi", hiragana: "び", katakana: "ビ", romaji: "bi" },
+      { name: "bu", hiragana: "ぶ", katakana: "ブ", romaji: "bu" },
+      { name: "be", hiragana: "べ", katakana: "ベ", romaji: "be" },
+      { name: "bo", hiragana: "ぼ", katakana: "ボ", romaji: "bo" },
+    ]
+  },
+  handakuon: {
+    pa: [
+      { name: "pa", hiragana: "ぱ", katakana: "パ", romaji: "pa" },
+      { name: "pi", hiragana: "ぴ", katakana: "ピ", romaji: "pi" },
+      { name: "pu", hiragana: "ぷ", katakana: "プ", romaji: "pu" },
+      { name: "pe", hiragana: "ぺ", katakana: "ペ", romaji: "pe" },
+      { name: "po", hiragana: "ぽ", katakana: "ポ", romaji: "po" },
+    ]
+  },
+  yoon: {
+    kya: [
+      { name: "kya", hiragana: "きゃ", katakana: "キャ", romaji: "kya" },
+      { name: "kyu", hiragana: "きゅ", katakana: "キュ", romaji: "kyu" },
+      { name: "kyo", hiragana: "きょ", katakana: "キョ", romaji: "kyo" },
+    ],
+    sha: [
+      { name: "sha", hiragana: "しゃ", katakana: "シャ", romaji: "sha" },
+      { name: "shu", hiragana: "しゅ", katakana: "シュ", romaji: "shu" },
+      { name: "sho", hiragana: "しょ", katakana: "シュ", romaji: "sho" },
+    ],
+    cha: [
+      { name: "cha", hiragana: "ちゃ", katakana: "チャ", romaji: "cha" },
+      { name: "chu", hiragana: "ちゅ", katakana: "チュ", romaji: "chu" },
+      { name: "cho", hiragana: "ちょ", katakana: "チョ", romaji: "cho" },
+    ],
+    nya: [
+      { name: "nya", hiragana: "にゃ", katakana: "ニャ", romaji: "nya" },
+      { name: "nyu", hiragana: "にゅ", katakana: "ニュ", romaji: "nyu" },
+      { name: "nyo", hiragana: "にょ", katakana: "ニョ", romaji: "nyo" },
+    ],
+    mya: [
+      { name: "mya", hiragana: "みゃ", katakana: "ミャ", romaji: "mya" },
+      { name: "myu", hiragana: "みゅ", katakana: "ミュ", romaji: "myu" },
+      { name: "myo", hiragana: "みょ", katakana: "ミョ", romaji: "myo" },
+    ],
+    rya: [
+      { name: "rya", hiragana: "りゃ", katakana: "リャ", romaji: "rya" },
+      { name: "ryu", hiragana: "りゅ", katakana: "リュ", romaji: "ryu" },
+      { name: "ryo", hiragana: "りょ", katakana: "リョ", romaji: "ryo" },
+    ],
+    gya: [
+      { name: "gya", hiragana: "ぎゃ", katakana: "ギャ", romaji: "gya" },
+      { name: "gyu", hiragana: "ぎゅ", katakana: "ギュ", romaji: "gyu" },
+      { name: "gyo", hiragana: "ぎょ", katakana: "ギョ", romaji: "gyo" },
+    ],
+    ja: [
+      { name: "ja", hiragana: "じゃ", katakana: "ジャ", romaji: "ja" },
+      { name: "ju", hiragana: "じゅ", katakana: "ジュ", romaji: "ju" },
+      { name: "jo", hiragana: "じょ", katakana: "ジョ", romaji: "jo" },
+    ],
+    bya: [
+      { name: "bya", hiragana: "びゃ", katakana: "ビャ", romaji: "bya" },
+      { name: "byu", hiragana: "びゅ", katakana: "ビュ", romaji: "byu" },
+      { name: "byo", hiragana: "びょ", katakana: "ビョ", romaji: "byo" },
+    ],
+    pya: [
+      { name: "pya", hiragana: "ぴゃ", katakana: "ピャ", romaji: "pya" },
+      { name: "pyu", hiragana: "ぴゅ", katakana: "ピュ", romaji: "pyu" },
+      { name: "pyo", hiragana: "ぴょ", katakana: "ピョ", romaji: "pyo" },
+    ],
+  }
 }
